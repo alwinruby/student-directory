18830f55ff1012728a4f38b5204cc46308799816
alwinsolanky <alwinsolanky@gmail.com>
2016-06-23 15:51:09 +0100

    Ex 8.7 In the input_students method the cohort value is hard-coded. How can you ask for both the name and the cohort? What if one of the values is empty? Can you supply a default value? The input will be given to you as a string? How will you convert it to a symbol? What if the user makes a typo?

--- directory.rb
+++ directory.rb
@@ -18,36 +18,43 @@ def print_footer(names)
 end
 
 def input_students
+  # New array of Months
+  months = ['January', 'February', 'March',
+            'April', 'May', 'June', 'July',
+            'August', 'September', 'October',
+            'November', 'December']
   # create an empty array
   students = []
   name = 'Alan'
-
+  default = 'July'
   # while the name is not empty, repeat this code
   until name.empty?
-    puts "Please enter the names of the student"
+    puts 'Please enter the names of the student'
     # get the first name
     name = gets.chomp.capitalize
     break if name.empty?
-    puts "Please enter your hobbies"
+    puts 'Please enter your cohort (using full month)'
+    # get cohort Details
+    cohort = gets.chomp.capitalize
+    puts 'Please enter your hobbies'
     # get hobbies
     hobbies = gets.chomp.capitalize
-    puts "Please enter your country of birth"
+    puts 'Please enter your country of birth'
     # get the country
     birth_country = gets.chomp.capitalize
-    puts "Please enter your height"
+    puts 'Please enter your height'
     # get the height
     height = gets.chomp
 
-    puts "To finish, just hit return twice"
-
-    # add the student hash to the array
-    students << {name: name, cohort: :November, hobbies: hobbies, birth_country: birth_country, height: height}
+    if cohort.empty? or !months.include?(cohort)
+      cohort = default
+    end
+    puts 'To finish, just hit return twice'
+    students << {name: name, cohort: cohort, hobbies: hobbies, birth_country: birth_country, height: height}
     puts "Now we have #{students.count} students"
-
   end
-
-  # return the array of students
-  students
+   #Return array of students
+    students
 end
 
 students = input_students
