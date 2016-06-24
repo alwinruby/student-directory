94f36875b5d9006df75fe12b17146646c3b2fcd0
alwinsolanky <alwinsolanky@gmail.com>
2016-06-24 20:24:49 +0100

    Ex 9 Interactive Menu added

--- directory.rb
+++ directory.rb
@@ -79,8 +79,38 @@ def print_cohort(students)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
-print_cohort(students)
+#students = input_students
+#print_header
+#print(students)
+#print_footer(students)
+#print_cohort(students)
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Show the Cohort Students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "3"
+      print_cohort(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
